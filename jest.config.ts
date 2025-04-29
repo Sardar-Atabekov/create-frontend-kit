8d04a5f5180ad016f40b28cd11ebd4dea6677f45
Sardar_Atabekov <sardarbekatabekov@gmail.com>
2025-04-29 20:30:57 +0600

    fix configs errors and add styles

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest-transform-stub',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
 
