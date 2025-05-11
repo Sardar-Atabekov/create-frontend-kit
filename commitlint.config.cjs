1bfd61c672bd3e9516bede0653c37c8fdc7d2288
Sardar_Atabekov <sardarbekatabekov@gmail.com>
2025-05-11 23:51:42 +0600

    ci: update commitlint and clear ci

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 72],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+  },
+};
